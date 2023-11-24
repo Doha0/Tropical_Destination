e629a7a25deec4df5b0fef8903b20c030e4b8870
Doha0 <abu.jardoha@gmail.com>
2023-11-24 21:01:54 +0600

    navbar updated

--- src/Homepage/Banner/Banner.jsx
+++ src/Homepage/Banner/Banner.jsx
@@ -3,24 +3,26 @@ import bannerImage from '../../assets/Images/banner.png'
 
 const Banner = () => {
     return (
-        <section className="container px-6 py-16 mx-auto inter">
-            <div className="grid md:grid-cols-2 items-center">
-                <div className="w-full lg:w-1/2">
-                    <div className="lg:max-w-7xl">
-                        <p className='mb-2 font-semibold text-xl'>Discover the beauty of the tropics</p>
-                        <div className='space-y-12 py-6 poppins '>
-                            <h1 className="font-bold black-text-color text-3xl lg:text-[80px]">Tropical</h1>
-                            <h1 className="font-bold black-text-color text-3xl lg:text-[80px]">Destinations</h1>
-                            <h2 className='font-medium black-text-color text-3xl lg:text-[80px]'>For Student</h2>
+        <section className="container px-6 py-16 mx-auto inter max-h-screen  mb-10 lg:mb-40">
+            <div className="md:flex items-center justify-center md:gap-x-12 lg:gap-x-24">
+
+                <div className="w-full md:w-1/2 order-last md:order-first">
+                    <div className="md:max-w-xl">
+                        <p className='mb-1 font-semibold text-sm md:text-xl'>Discover the beauty of the tropics</p>
+                        <div className='space-y-2 lg:space-y-6 py-4 md:py-6 poppins'>
+                            <h1 className="font-bold black-text-color text-4xl md:text-5xl lg:text-[80px]">Tropical</h1>
+                            <h1 className="font-bold black-text-color text-4xl md:text-5xl lg:text-[80px]">Destinations</h1>
+                            <h2 className='font-medium black-text-color text-4xl md:text-5xl lg:text-[80px]'>For Student</h2>
                         </div>
-                        <p className="my-4 lg:my-12 text-gray-600 ">Lorem ipsum dolor sit amet consectetur. Sit mattis donec mi bibendum integer rutrum nisi. A nec nisl vitae </p>
+                        <p className="my-4 lg:my-12 text-gray-600 text-sm md:text-base">Lorem ipsum dolor sit amet consectetur. Sit mattis donec mi bibendum integer rutrum nisi. A nec nisl vitae </p>
                         <button className='bg-[#F27A44] text-white px-12 py-4 rounded-[30px] uppercase'>
                             Sign Up
                         </button>
                     </div>
                 </div>
-                <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
-                    <img className="w-full h-full lg:max-w-3xl" src={bannerImage} alt="bannerImg" />
+
+                <div className="flex items-center justify-center w-full mt-6 lg:mt-0 md:w-1/2 order-first md:order-last">
+                    <img className="w-full h-full md:max-w-xl" src={bannerImage} alt="bannerImg" />
                 </div>
             </div>
         </section >
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,78 +1,93 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from "../../assets/logo.png"
-import './Navbar.css'
+import React, { useState } from "react";
 
 const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
 
-    const navItems = <>
-        <div className="nav-items-list font-medium">
-            <Link className="nav-items">
-                Home
-            </Link>
-            <Link className="nav-items">
-                About
-            </Link>
-            <Link className="nav-items">
-                Schedules
-            </Link>
-            <Link className="nav-items">
-                Membership
-            </Link>
-            <Link className="nav-items">
-                Pricing
-            </Link>
-        </div>
+    const toggleSidebar = () => {
+        setSidebarOpen(!isSidebarOpen);
+    };
 
-        <div className="nav-items-list">
-            <Link className="nav-items text-[#7D7D7D]">
-                offers
-            </Link>
-            <Link className="nav-items">
-                <button className='primary-button'>
-                    courses
-                </button>
-            </Link>
-        </div>
-    </>
+    const navItems = ["Home", "About", "Schedules", "Membership", "Pricing"];
 
     return (
-        <nav x-data={{ isOpen }} className="relative bg-white">
-            <div className="px-6  mx-auto md:flex">
-                <div className="flex items-center justify-between">
-                    <Link>
-                        <img className="w-full h-12" src={logo} alt="logo" />
-                    </Link>
-
-
-                    <div className="flex lg:hidden">
+        <nav className="bg-white p-4">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Navbar items on the left side */}
+                <div className="flex items-center space-x-4">
+                    {/* Hamburger menu for small screens */}
+                    <div className="md:hidden">
                         <button
-                            onClick={() => setIsOpen(!isOpen)}
                             type="button"
-                            className="text-gray-500  hover:text-gray-600  focus:outline-none focus:text-gray-600 "
-                            aria-label="toggle menu"
+                            className="text-gray-700 focus:outline-none"
+                            onClick={toggleSidebar}
                         >
-                            {!isOpen ? (
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M4 8h16M4 16h16" />
-                                </svg>
-                            ) : (
-                                <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            )}
+                            <svg
+                                className="h-6 w-6"
+                                fill="none"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path d="M4 6h16M4 12h16m-7 6h7"></path>
+                            </svg>
                         </button>
+                    </div>
+
+                    {/* Navbar items for medium and large screens */}
+                    <div className="hidden md:flex space-x-4">
+                        {navItems.map((item, index) => (
+                            <div key={index} className="text-gray-800 hover:text-gray-900">
+                                {item}
+                            </div>
+                        ))}
                     </div>
                 </div>
 
-                <div
-                    className={`absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white  md:mt-0 md:p-0 md:top-0 md:relative md:opacity-100 ${isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full'} md:translate-x-0 md:flex md:items-center md:justify-between`}
-                >
-                    {
-                        navItems
-                    }
+                {/* Buttons on the right side */}
+                <div className="flex items-center space-x-4">
+                    <button className="text-gray-600">Offers</button>
+                    <button className="my-btn">
+                        Courses
+                    </button>
                 </div>
+
+                {/* Sidebar for small screens */}
+                {isSidebarOpen && (
+                    <div className="md:hidden fixed inset-0 bg-[#F27A44] z-50">
+                        <div className="flex justify-end p-4">
+                            <button
+                                type="button"
+                                className="text-white focus:outline-none"
+                                onClick={toggleSidebar}
+                            >
+                                <svg
+                                    className="h-6 w-6"
+                                    fill="none"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                >
+                                    <path d="M6 18L18 6M6 6l12 12"></path>
+                                </svg>
+                            </button>
+                        </div>
+                        <ul className="flex flex-col items-start p-4 space-y-4">
+                            {navItems.map((item, index) => (
+                                <li
+                                    key={index}
+                                    className="text-white hover:text-gray-300 cursor-pointer"
+                                    onClick={toggleSidebar}
+                                >
+                                    {item}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
             </div>
         </nav>
     );
