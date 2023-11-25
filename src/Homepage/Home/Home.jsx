f1106d63b12c14f620e853edc6bbedd01d713bdc
Doha0 <abu.jardoha@gmail.com>
2023-11-25 12:40:06 +0600

    destination added

--- src/Homepage/Home/Home.jsx
+++ src/Homepage/Home/Home.jsx
@@ -3,14 +3,18 @@ import Banner from '../Banner/Banner';
 import Advanture from '../Advanture/Advanture';
 import Discounts from '../Discounts/Discounts';
 import Bookings from '../Bookings/Bookings';
+import Testimonials from '../Testimonials/Testimonials';
+import Destination from '../Destination/Destination';
 
 const Home = () => {
     return (
         <section>
             <Banner />
             <Advanture />
+            <Destination/>
             <Discounts />
             <Bookings />
+            <Testimonials />
         </section>
     );
 };
